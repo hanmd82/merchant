d76afbb34ea644098ecd42847f720477df421938
Mingding <hanmd82@gmail.com>
2015-10-26 01:20:24 +0800

    Add steps details for passing cukes

--- features/step_definitions/shopping_cart_steps.rb
+++ features/step_definitions/shopping_cart_steps.rb
@@ -1,19 +1,31 @@
-Given(/^the user is at the products catalogue page$/) do
-  pending # express the regexp above with the code you wish you had
+Given(/^there is a 'Oranges' item in the products list$/) do
+  Product.create(title: "Oranges", price: 2.99, description: "Bag of 6 Valencia oranges", image_url: "oranges.jpg", stock: 100)
 end
 
-When(/^the user clicks on the Oranges link$/) do
-  pending # express the regexp above with the code you wish you had
+When(/^the user goes to the products URL$/) do
+  visit products_path
 end
 
-Then(/^the user should see the Oranges item details$/) do
-  pending # express the regexp above with the code you wish you had
+Then(/^the user should see the 'Oranges' item in the product catalogue$/) do
+  expect(page).to have_content 'Oranges'
 end
 
-Then(/^the user should see the back URL$/) do
-  pending # express the regexp above with the code you wish you had
+When(/^the user clicks on the 'Oranges' link$/) do
+  click_link 'Oranges'
+end
+
+Then(/^the user should see the 'Oranges' item details$/) do
+  expect(page).to have_content 'Title: Oranges'
+  expect(page).to have_content 'Price: $2.99'
+  expect(page).to have_content 'Description: Bag of 6 Valencia oranges'
+  expect(page).to have_content 'Stock: 100 In Stock'
+end
+
+Then(/^the user should see the 'Back' link$/) do
+  expect(page).to have_content 'Back'
 end
 
 Then(/^the user should be able to go back to the products catalogue page$/) do
-  pending # express the regexp above with the code you wish you had
+  click_link 'Back'
+  expect(current_path).to eq(products_path)
 end
